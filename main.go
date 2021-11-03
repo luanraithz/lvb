801f3ebc71e635aee247d137c206d48143c88a68
Luan Raithz Machado <luanraithz@gmail.com>
2021-11-03 16:49:03 -0300

    removed goroutine

--- main.go
+++ main.go
@@ -20,36 +20,33 @@ func main() {
 
 	r, err := cmd.StdoutPipe()
 	must(err)
-	done := make(chan bool)
-	scanner := bufio.NewScanner(r)
-	branchMap := map[string]bool{} // Wanted a set
-	go func() {
-		counter := 0
-		for scanner.Scan() && counter < 10000 {
-			line := scanner.Text()
-			if strings.Contains(line, "checkout: moving from") {
-				branches := strings.Split(strings.Split(line, "checkout: moving from")[1], "to")
-				for _, b := range branches {
-					trimmed := strings.TrimSpace(b)
-					if trimmed != "" {
-						branchMap[trimmed] = true
-					}
-				}
-			}
-			counter++
-		}
-		done <- true
-
-	}()
 	stderr, err := cmd.StderrPipe()
 	must(err)
 	err = cmd.Start()
+
 	if err != nil {
 		println(bufio.NewScanner(r).Text())
 		println(bufio.NewScanner(stderr).Text())
 		os.Exit(1)
 	}
-	<-done
+
+	branchMap := map[string]bool{} // Wanted a set
+	scanner := bufio.NewScanner(r)
+	counter := 0
+	for scanner.Scan() && counter < 10000 {
+		line := scanner.Text()
+		if strings.Contains(line, "checkout: moving from") {
+			branches := strings.Split(strings.Split(line, "checkout: moving from")[1], "to")
+			for _, b := range branches {
+				trimmed := strings.TrimSpace(b)
+				if trimmed != "" {
+					branchMap[trimmed] = true
+				}
+			}
+		}
+		counter++
+	}
+
 	must(cmd.Wait())
 	branches := []string{}
 	for k := range branchMap {
